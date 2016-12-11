1261970601be2b901a5dfc45fbe8e666d2f653a6
tobq <tobi.akiny@gmail.com>
2016-12-11 17:31:10 +0000

    update

--- jq.js
+++ jq.js
@@ -1,1 +1,96 @@
-function next(){($("body").height()-($(window).scrollTop()+$(window).height()-100)>0||3>i)&&i<$("section").length-1&&i>=-1&&scr&&(0==i&&$("#goc").css("opacity","1"),scr=!1,$("html, body").stop().animate({scrollTop:$($("section")[++i]).offset().top-100},700,"easeOutCubic"),setTimeout(function(){scr=!0},700))}function prev(){i<$("section").length&&i>0&&scr&&(1==i&&$("#goc").css("opacity","0"),scr=!1,$("html, body").stop().animate({scrollTop:$($("section")[--i]).offset().top-100},700,"easeOutCubic"),setTimeout(function(){scr=!0},700))}function now(){scr&&(scr=!1,$("html, body").stop().animate({scrollTop:$($("section")[i]).offset().top-100},700,"easeOutCubic"),setTimeout(function(){scr=!0},700))}function displaywheel(e){if(scr){var t=window.event||e,o=t.detail?-120*t.detail:t.wheelDelta;0>o?next():prev(),e.preventDefault(),e.stopPropagation()}}var i=0,Tim,scr=!0;$(window).resize(function(){clearTimeout(Tim),Tim=setTimeout(function(){now()},500)});var mousewheelevt=/Firefox/i.test(navigator.userAgent)?"DOMMouseScroll":"mousewheel";document.attachEvent?document.attachEvent("on"+mousewheelevt,displaywheel):document.addEventListener&&document.addEventListener(mousewheelevt,displaywheel,!1),$("body").swipe({swipeUp:function(e,t,o,i){next()},swipeDown:function(e,t,o,i){prev()},threshold:100,allowPageScroll:"vertical"}),$("#god").click(function(){next()}),$("#gou").click(function(){prev()}),$("#proj").click(function(){next()});$(window).on('beforeunload', function() {$(window).scrollTop(0);});now();
+function next() {
+	if ($("body").height() - ($(window).scrollTop() + $(window).height() - 100) > 0 || 3 > i) {
+		if (i < $("section").length - 1) {
+			if (i >= -1) {
+				if (scr) {
+					if (0 == i) {
+						$("#goc").css("opacity", "1");
+						$("#h").css("background","#000");
+					}
+					scr = false;
+					$("html, body").stop().animate({
+						scrollTop : $($("section")[++i]).offset().top - 100
+					}, 700, "easeOutCubic");
+					setTimeout(function() {
+						scr = true;
+					}, 700);
+				}
+			}
+		}
+	}
+}
+function prev() {
+	if (i < $("section").length) {
+		if (i > 0) {
+			if (scr) {
+				if (1 == i) {
+					$("#goc").css("opacity", "0");
+					$("#h").css("background","rgba(0,0,0,0)");
+					$("html, body").stop().animate({
+						scrollTop : $($("section")[--i]).offset().top
+					}, 700, "easeOutCubic");
+				} else $("html, body").stop().animate({
+					scrollTop : $($("section")[--i]).offset().top - 100
+				}, 700, "easeOutCubic");
+				scr = false;
+				setTimeout(function() {
+					scr = true;
+				}, 700);
+			}
+		}
+	}
+}
+function now() {
+	if (scr) {
+		scr = false;
+		$("html, body").stop().animate({
+			scrollTop : $($("section")[i]).offset().top - 100
+		}, 700, "easeOutCubic");
+		setTimeout(function() {
+			scr = true;
+		}, 700);
+	}
+}
+function displaywheel(e) {
+	if (scr) {
+		var evt = window.event || e;
+		var o = evt.detail ? -120 * evt.detail : evt.wheelDelta;
+		if (0 > o) {
+			next();
+		} else {
+			prev();
+		}
+		e.preventDefault();
+		e.stopPropagation();
+	}
+}
+var i = 0;
+var Tim;
+var scr = true;
+$(window).resize(function() {
+	clearTimeout(Tim);
+	Tim = setTimeout(function() {
+		now();
+	}, 500);
+});
+var mousewheelevt = /Firefox/i.test(navigator.userAgent) ? "DOMMouseScroll" : "mousewheel";
+document.attachEvent ? document.attachEvent("on" + mousewheelevt, displaywheel) : document.addEventListener && document.addEventListener(mousewheelevt, displaywheel, false), $("body").swipe({
+	swipeUp : function(e, dataAndEvents, deepDataAndEvents, ignoreMethodDoesntExist) {
+		next();
+	},
+	swipeDown : function(e, dataAndEvents, deepDataAndEvents, ignoreMethodDoesntExist) {
+		prev();
+	},
+	threshold : 100,
+	allowPageScroll : "vertical"
+}), $("#god").click(function() {
+	next();
+}), $("#gou").click(function() {
+	prev();
+}), $("#proj").click(function() {
+	next();
+});
+$(window).on("beforeunload", function() {
+	$(window).scrollTop(0);
+});
+now();
