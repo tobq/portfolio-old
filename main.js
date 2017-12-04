07041001071312860ec491e1f6c63ce7b3f5df73
Tobi <tobi.akiny@gmail.com>
2017-12-04 00:15:26 +0000

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var gou = document.getElementById("gou");
 var projects = document.getElementById("proj");
 var slide = document.getElementById("slide");
 var sections = document.getElementsByTagName("section"), focused = 0;
-var scrolling = false, SCROLL_DUR = 700;
+var scrolling = false, SCROLL_DUR = 500;
 
 window.onwheel = function MouseWheelHandler(e) {
 	var e = window.event || e;
@@ -18,17 +18,19 @@ god.onclick = projects.onclick = next;
 gou.onclick = prev;
 
 function next() {
+	if (focused + 2 > sections.length) return false;
 	++focused;
 	setTop();
 }
 
 function prev() {
+	if (focused < 1) return false;
 	--focused;
 	setTop();
 }
 
 function setTop() {
-	if (scrolling || focused < 0 || focused + 1 > sections.length) return false;
+	if (scrolling) return false;
 	scrolling = true;
 	slide.style.bottom = focused * window.innerHeight + "px";
 	projects.className = goc.className = focused ? "o" : "";
